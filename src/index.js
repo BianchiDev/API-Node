3357d95f882cecc33a899782b7328083ac2ad59b
BianchiDev <ifpebianchi@gmail.com>
2024-05-26 02:14:06 -0300

    Melhorando os métodos e acrescentando Middlewares

--- src/index.js
+++ src/index.js
@@ -5,64 +5,73 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 app.use(express.json())
 
+
 const projects = []
 
-// Imprime o diretório atual no console para depuração
-console.log('Diretório atual:', __dirname);
+function logRoutes(request, response, next){
+    const {method, url} = request
+    const route =`[${method.toUpperCase()}] ${url}`
+    console.log(route)
+    return next()
+}
+app.use(logRoutes)
 
-// Imprime o caminho completo do arquivo index.js no console para depuração
-console.log('Arquivo index.js encontrado em:', __filename);
-
-// Define uma rota para o caminho raiz ('/'). Quando uma solicitação GET for feita a essa rota,
-// a função de callback será executada.
 app.get('/projects', function(request, response){
-    // Envia a resposta "Olá Bianchi!" quando a rota raiz for acessada
-    const {title, owner, page} = request.query //trabalhando com request query que paramentros de consulta.
-    console.log(title, owner, page)
-    return response.json([
-        'projeto 1',
-        'projeto 2'
-    ]
-    )
+    return response.json(projects)
 });
 
 // Método POST para criar um novo projeto
 app.post('/projects', function(request, response){
-    // Retorna uma lista de projetos em formato JSON
-    // Neste exemplo, a lista de projetos é fixa e não considera dados da requisição
-    const {name, owner} = request.body //parametros de rota poderia ser no put
-    console.log(name, owner)// Os valores recebidos por rotas será sempre
-    return response.json([
-        'projeto 1',
-        'projeto 2',
-        'projeto 3'
-    ]);
+       const {name, owner} = request.body //parametros de rota poderia ser no put
+ const project = {
+    id: uuidv4(),
+    name,
+    owner
+ }
+    projects.push(project)
+
+    return response.status(201).json(project)
 });
 
 // Método PUT para atualizar um projeto existente identificado pelo ID
 app.put('/projects/:id/', function(request, response){
-    // O ID do projeto é obtido através dos parâmetros da URL (:id)
-    // Retorna uma lista de projetos em formato JSON
-    // Neste exemplo, a lista de projetos é fixa e representa a atualização de um projeto
-    const {id} = request.params //parametros de rota
+      const {id} = request.params //parametros de rota
     const {name, owner} = request.body //Recebendo os parametros via Json
-    console.log(id, name, owner)// Os valores recebidos por rotas será sempre
-    return response.json([
-        'projeto 4',
-        'projeto 2',
-        'projeto 3'
-    ]);
+    
+    const projectIndex = projects.findIndex(p => p.id === id)
+
+    if(projectIndex < 0){
+       return response.status(404).json({error: 'Project not found' }) 
+    }
+    
+    if(!name || !owner){
+        return response.status(400).json({error: 'Name and owner are required' }) 
+    }
+
+    const project = {
+        id, 
+        name,
+        owner
+    }
+
+    projects[projectIndex] = project
+
+    return response.json(project)
 });
 
 // Método DELETE para deletar um projeto existente identificado pelo ID
 app.delete('/projects/:id', function(request, response){
-    // O ID do projeto é obtido através dos parâmetros da URL (:id)
-    // Retorna uma lista de projetos em formato JSON
-    // Neste exemplo, a lista de projetos é fixa e representa a exclusão de um projeto
-    return response.json([
-        'projeto 2',
-        'projeto 3'
-    ]);
+    const {id} = request.params //parametros de rota
+
+    const projectIndex = projects.findIndex(p => p.id === id)
+
+    if(projectIndex < 0){
+       return response.status(404).json({error: 'Project not found' }) 
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
 });
 
 
--- touch README.md
+++ touch README.md
@@ -1,8 +1,11 @@
 <h1 align="center">API-Node</h1>
+<hr>
 
 ## Desenvolvendo os principais conceitos para criação APIs Restful com NodeJS e Typescript e alguns projetos.
+<hr>
 
  ` Esse projeto consiste em uma aplicação backend com funcionalidades para criação de cadastro de usuários (users), que inclui um relacionamento com o cadastro de funções (roles), autenticação com token de acesso e token de atualização, atualização de perfil e atualização de imagem de avatar através de upload.  `
+ <br>
 
 - Conceitos básicos de API Restful
 
@@ -18,18 +21,42 @@
 
 - Validação de dados de requisições
 
+ <hr>
+
+ ## Ferramentas necessárias para esse projeto!!!
+ <hr>
+
+- Navegador de internet
+
+- Terminal de shell
+
+- Git
+
+- NodeJS versão 16 ou posterior
+
+- Visual Studio Code ou similar
+
+- Insomnia, Postman ou similar
+<hr>
+
+## Configunração do ambiente.
+<hr>
+
+` Criando o package.json` <br> 
+``` npm init -y ```
+
+` Instalando o express` <br>
+``` npm intall express --save ```
 
 
+` Atualizar os pacotes e dependências` <br>
+``` npm update ```
 
- ### Ferramentas necessárias para esse projeto!!!
-Navegador de internet
+` Instalando o nodemon e configurando para LiveReload `
+``` npm i -D nodemon ```
 
-Terminal de shell
+` Dentro do arquivo package.json, detro de "scripts" colocar o script de start`
 
-Git
-
-NodeJS versão 16 ou posterior
-
-Visual Studio Code ou similar
-
-Insomnia, Postman ou similar
+```"start": "nodemon src/index.js"```
+` Para subir a plicação basta rodar o seguinte comando:`
+```npm start```
